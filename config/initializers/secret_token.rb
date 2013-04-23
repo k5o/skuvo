03bcf54b4c126caa014ed71aaaf702905ee575cd
Kevin Ko <enhasa@gmail.com>
2013-04-22 23:41:33 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skuvo::Application.config.secret_token = 'f0e658d70587ae1ab9e77ef62e73e0d2efcf9b9af12bd1d1cc07e0e586e460522a4406ee478750bb6c8addcbb66260465ce8a5cbd734b18fc06820cc904d3e6c'
