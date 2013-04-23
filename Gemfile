d7caa5ad3b463a5b8769f62a2709796ee2520398
Kevin Ko <enhasa@gmail.com>
2013-04-23 15:30:09 -0700

    add jquery upload

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '3.2.13'
 
 gem 'pg'
 gem 'bcrypt-ruby'
+gem "jquery-fileupload-rails"
 gem 'carrierwave'
 gem 'cloudinary'
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
 	attr_accessible :username, :email, :password
 	has_secure_password
 	has_many :photos
-
+	
 end
