98c8dba81b88cc4e604bfc79bb6803fd0154a7a5
Kevin Ko <enhasa@gmail.com>
2013-04-23 17:19:52 -0700

    javascript workflow (render success) completed

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,8 +1,11 @@
 class PhotosController < ApplicationController
 	def create
-		photo = Photo.new
-		photo.url = params['image']
-		photo.save
-		redirect_to root_path
+		@photo = Photo.new
+		@photo.url = params['image']
+		if @photo.save
+			render 'success'
+		else
+			render 'error'
+		end
 	end
 end
