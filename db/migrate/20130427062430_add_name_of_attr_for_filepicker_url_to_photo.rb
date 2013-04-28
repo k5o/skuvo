bcff3239753ae8ab14859b8ed7d55f473f500347
Kevin Ko <enhasa@gmail.com>
2013-04-28 13:40:10 -0700

    housekeeping

--- db/migrate/20130427062430_add_name_of_attr_for_filepicker_url_to_photo.rb
+++
@@ -1,9 +0,0 @@
-class AddNameOfAttrForFilepickerUrlToPhoto < ActiveRecord::Migration
-    def up
-        add_column :photos, :filepicker_url, :string
-    end
-
-    def down
-        remove_column :photos, :filepicker_url
-    end
-end
