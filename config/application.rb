1477f9e70ae1276d3a44f84ed80632d85f7a9d5b
Kevin Ko <enhasa@gmail.com>
2013-04-29 11:43:01 -0700

    added development status to codes

--- config/application.rb
+++ config/application.rb
@@ -60,8 +60,10 @@ module Skuvo
     config.assets.version = '1.0'
 
     ## For Filepicker
-    filepicker_code = YAML.load(File.read(Rails.root.join('config', 'initializers' ,'a-codes.yml')))
-    config.filepicker_rails.api_key = filepicker_code
+    if development?
+      filepicker_code = YAML.load(File.read(Rails.root.join('config', 'initializers' ,'a-codes.yml')))
+      config.filepicker_rails.api_key = filepicker_code
+    end
     
     ## To get rid of field_with_errors wrappers
     config.action_view.field_error_proc = Proc.new { |html_tag, instance| 
