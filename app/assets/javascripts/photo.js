86d90f7627a6d0b62f59284252793b8db98d2142
Kevin Ko <enhasa@gmail.com>
2013-04-30 12:51:12 -0700

    fix ajax deletion after change to a#delete

--- app/assets/javascripts/photo.js
+++ app/assets/javascripts/photo.js
@@ -1,19 +1,23 @@
 var Photo = {
   init: function() {
-  	$('.photoButtons').on('click', 'form.button_to', this.confirmDelete);    
-    $('.photoButtons').on('ajax:success', 'form.button_to', this.deletePhoto);
-    $('.photoButtons').on('ajax:success', 'form.button_to', this.subtractPhotoCount);
+  	$('.photoButtons').on('click', 'a', this.confirmDelete);
+    $('.photoButtons').on('ajax:success', 'a', this.deletePhoto);
+    $('.photoButtons').on('ajax:success', 'a', this.subtractPhotoCount);
   },
 
   confirmDelete: function() {
+    console.log("A")
     if (!confirm("Delete this photo?")) return false;
   },
 
   deletePhoto: function() {
+    console.log("B")
+    debugger
     $(this).parent().parent('.photoView').fadeOut(800);
   },
 
   subtractPhotoCount: function() {
+    console.log("C")
     newVal = parseInt($('#photosCount').text());
     $('#photosCount').text(newVal - 1);
   }
