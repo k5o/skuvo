486b2db89964b837adf08de7338e2731e9a33dd5
Kevin Ko <enhasa@gmail.com>
2013-05-04 13:59:48 -0700

    revise unit spec

--- spec/models/albums_spec.rb
+++ spec/models/albums_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Album do
   it { should respond_to :url }
   it { should belong_to(:user) }
-  it { should have_many(:photos) }
+  it { should have_many(:photos).dependent(:destroy) }
 
   describe "model actions" do
   	it "should create a url before saving to db" do
