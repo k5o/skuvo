f079e81290a40e80bd73e8a5f1b82f408d271f61
Kevin Ko <enhasa@gmail.com>
2013-05-04 20:38:11 -0700

    cleanup gallery conditionals

--- spec/models/albums_spec.rb
+++ spec/models/albums_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Album do
   it { should respond_to :url }
   it { should belong_to(:user) }
-  it { should have_many(:photos).dependent(:destroy) }
+  it { should have_many(:photos) }
 
   describe "model actions" do
   	it "should create a url before saving to db" do
